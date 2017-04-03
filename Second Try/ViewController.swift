4cd84fe635085a0be4fde81d6d988c5fbfc1fef2
Diego <digomsolano@hotmail.com>
2017-04-02 22:20:40 -0600

    First Commit

--- Second Try/ViewController.swift
+++ Second Try/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var Salute: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func PressNext(_ sender: AnyObject) {
+   Salute.text = "This is the second try at building an Application by DMS"
+        print("Button tapped")
+    tapCount = tapCount + 1
+        if tapCount >= 10 {
+            Salute.text = "You've pressed Next over 10 times"
+        }
+        
+        // Hay que poner el if dentro del codigo de la accion del boton (PressNext) para que sirva.
+
+    }
+    
+    
+    @IBAction func pressedSB(_ sender: Any) {
+        
+        Salute.text = "Now you've pressed the second button"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
